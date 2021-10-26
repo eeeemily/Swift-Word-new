e9162fbf01ec4aa375b4c0e01b00236d1f43322d
Minghui Zheng <mzheng9@u.rochester.edu>
2021-10-25 22:53:36 -0400

    Initial Commit

---
+++ Word/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Word
+//
+//  Created by Zheng, Minghui on 10/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
