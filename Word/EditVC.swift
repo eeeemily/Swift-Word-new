e58e82bc2de06d52467f81c349df882aea90ae08
Emily Zheng <mzheng9@u.rochester.edu>
2021-11-10 00:07:47 -0500

    Added alert

--- Word/EditVC.swift
+++ Word/EditVC.swift
@@ -25,21 +25,21 @@ class EditVC: UIViewController {
         if let entity = NSEntityDescription.entity(forEntityName: "Entry", in: context) {
             let entry = NSManagedObject(entity: entity, insertInto: context)
             entry.setValue(title, forKeyPath: "title")
-            if(password.count<4){
-                print("password is too short!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                invalidPasswordAlert(controller: self)
-                if(autoPword.count>7){
-                    print("****************************")
-                    print(autoPword)
-                    print("****************************")
-
-                    entry.setValue(password, forKeyPath: "password")
-                    entry.setValue(pword.testPwd(pwd: password), forKey: "strength")
-                }
-            }else{
-                entry.setValue(password, forKeyPath: "password")
-                entry.setValue(pword.testPwd(pwd: password), forKey: "strength")
-            }
+//            if(password.count<4){
+//                print("password is too short!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+//                invalidPasswordAlert(controller: self)
+//                if(autoPword.count>7){
+//                    print("****************************")
+//                    print(autoPword)
+//                    print("****************************")
+//
+//                    entry.setValue(password, forKeyPath: "password")
+//                    entry.setValue(pword.testPwd(pwd: password), forKey: "strength")
+//                }
+//            }
+            entry.setValue(password, forKeyPath: "password")
+            entry.setValue(pword.testPwd(pwd: password), forKey: "strength")
+       
             
             
             do {
@@ -53,32 +53,33 @@ class EditVC: UIViewController {
     func invalidPasswordAlert(controller: UIViewController) {
         let alertMsg = "Your password is invalid. Would you like to have an auto generated password?"
         let alert = UIAlertController(title: "Warning", message: alertMsg, preferredStyle: .actionSheet)
-        
-        let autoGenerateAction = UIAlertAction(title: "Auto Generate", style: .cancel, handler:  { [self] (_) in
-            autoPword = pword.generatePsd()
+
+        let autoGenerateAction = UIAlertAction(title: "Auto Generate", style: .default, handler:  { [self] (_) in
+            passwordTextfield.text? = pword.generatePsd()
         })
-//        let changeAction = UIAlertAction(title: "Change Password", style: .default, handler: { (_) in
-//            print("User click change button")
-//        })
-        let cancelAction = UIAlertAction(title: "Dismiss", style: .destructive)
+        let cancelAction = UIAlertAction(title: "Change Password by myself", style: .cancel)
         
         alert.addAction(autoGenerateAction)
-//        alert.addAction(changeAction)
         alert.addAction(cancelAction)
         
-//        alert.popoverPresentationController?.permittedArrowDirections = []
-//        alert.popoverPresentationController?.sourceView = self.view
-//        alert.popoverPresentationController?.sourceRect = CGRect(x: self.view.frame.midX, y: self.view.frame.midY, width: 0, height: 0)
-//
         present(alert, animated: true)
     }
     
-    func autoGenerate(){
+    @IBAction func checkPassword(_ sender: Any) {
+        if((passwordTextfield.text?.description.count)!<4){
+            invalidPasswordAlert(controller: self)
+        }else{
+            let alertSheet = UIAlertController(title: "You password is valid", message: "Your password has more than 4 character!", preferredStyle: .actionSheet)
+            alertSheet.addAction(UIAlertAction(title: "Continue?", style:.cancel))
+            present(alertSheet, animated: true, completion: nil)
+        }
         
+
     }
+
     @IBAction func onCancel2(_ sender: Any) {
         presentingViewController?.dismiss(animated: true)
-
+        
     }
     
     @IBAction func onAdd2(_ sender: Any) {
